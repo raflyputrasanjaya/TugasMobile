1d271d9496534e77a479c56acfa8dfdb66cd74b4
raflyputrasanjaya <raflyputrasanjaya@gmail.com>
2025-12-19 16:23:58 +0700

    week 2

--- main.dart
+++ main.dart
@@ -1,29 +1,21 @@
+class Barang {
+  String nama;
+  int harga;
+
+  Barang(this.nama, this.harga);
+}
+
 void main() {
-  var nama = "Rafly";
-  int umur = 18;
-  const String jurusan = "RPL";
-  final String kampus = "Telkom University";
+  List<Barang> daftarBarang = [
+    Barang("Susu", 15000),
+    Barang("Roti", 12000),
+    Barang("Telur", 18000),
+  ];
 
-  if (umur >= 17 && jurusan == "RPL") {
-    print("Lolos syarat mahasiswa");
-  } else if (umur >= 17 || jurusan == "Informatika") {
-    print("Dipertimbangkan");
-  } else {
-    print("Tidak lolos");
+  int totalBayar = 0;
+  for (var barang in daftarBarang) {
+    totalBayar += barang.harga;
   }
 
-  for (int i = 1; i <= 3; i++) {
-    cetakStatus(nama: nama, semester: i);
-  }
-
-  int hasil = hitungSKS(nilai: 20);
-  print("Total SKS: $hasil");
-}
-
-void cetakStatus({required String nama, required int semester}) {
-  print("Mahasiswa $nama ada di semester $semester");
-}
-
-int hitungSKS({required int nilai}) {
-  return nilai * 2;
+  print("Total bayar = Rp $totalBayar");
 }
